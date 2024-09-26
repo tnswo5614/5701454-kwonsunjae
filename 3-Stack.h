c41750945ac31b066484fdb67e9e4eb40324ca31
tnswo5614 <tnswo5614@naver.com>
2024-09-26 23:39:09 +0900

    Add files via upload

---
+++ 3-Stack.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "LevelTraversal.h"
+typedef TreeNode* element;
+typedef struct StackNode {
+	element data;
+	struct StackNode* link;
+} StackNode;
+
+typedef struct {
+	StackNode* top;
+} LinkedStackType;
+
+extern void init(LinkedStackType* s);
+extern int is_empty(LinkedStackType* s);
+extern int is_full(LinkedStackType* s);
+extern void push(LinkedStackType* s, element item);
+extern void print_stack(LinkedStackType* s);
+extern element pop(LinkedStackType* s);
